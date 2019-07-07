aaeeac51f3c2a72a334041b615538196fc92dfca
sethitarandeep <sethi.tarandeep@gmail.com>
2019-07-08 06:37:22 +1000

    app init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-
+import { UserProfileComponent } from './user-profile/user-profile.component';
+import { AddressAutocompleteComponent } from './address-autocomplete/address-autocomplete.component';
+import { AppMaterialModule } from './app-material/app-material.module';
+import { MapsAutocompleteComponent } from './maps-autocomplete/maps-autocomplete.component';
+import {MatCardModule} from '@angular/material';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserProfileComponent,
+    AddressAutocompleteComponent,
+    MapsAutocompleteComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    AppMaterialModule,
+    FormsModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
